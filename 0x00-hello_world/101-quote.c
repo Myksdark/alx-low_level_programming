4d5ca838178b7b72ed8130269911bd67528da9db
Myksdark <myksdark@gmail.com>
2023-03-16 12:56:13 -0700

    UNIX is basically a simple operating system, but you have to be a genius to understand the simplicity

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,11 +6,10 @@
  * Description: Prints a message to the standard error stream
  * Return: Always 1 (Error)
  */
+
 int main(void)
 {
-const char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	
+const char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";	
 write(2, message, sizeof(message) - 1);
-
 return (1);
 }
