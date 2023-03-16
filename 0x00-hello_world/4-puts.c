045da17661679668c2fc454f73e572d32bc9d5e8
Myksdark <myksdark@gmail.com>
2023-03-16 10:55:08 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,12 +2,12 @@
 
 /**
  * main - Entry point of the program
- *
  * Return: 0 on success
  */
 int main(void)
-{
-    puts("Programming is like building a multilingual puzzle");
-    return (0);
-}
 
+{
+    puts("\"Programming is like building a multilingual puzzle");
+    return (0);
+
+}
