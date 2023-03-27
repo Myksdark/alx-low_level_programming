3629b09fa148141b34c232cd9b56fa7862e57452
Myksdark <myksdark@gmail.com>
2023-03-27 19:09:41 +0000

    Don't hate the hacker, hate the code

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,25 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <string.h>
 
-#define PASSWORD_LENGTH 10
+/**
+ * main - program that generates random valid
+ * passwords for the program 101-crackme
+ *
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
 {
-    char password[PASSWORD_LENGTH + 1];
-    const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-    int i;
+        int pass[100];
+        int i, sum, n;
 
-    srand(time(NULL));
+        sum = 0;        
+        srand(time(NULL));
+        for (i = 0; i < 100; i++)
+        {
+                pass[i] = rand() % 78;
+                sum += (pass[i] + '0');
+                putchar(pass[i] + '0');
+                if ((2772 - sum) - '0' < 78)
+                {
+                        n = 2772 - sum - '0';
+                        sum += n;
+                        putchar(n + '0');
+                        break;
+                }
+        }
 
-    for (i = 0; i < PASSWORD_LENGTH; i++) {
-        password[i] = charset[rand() % strlen(charset)];
-    }
-
-    password[PASSWORD_LENGTH] = '\0';
-
-    printf("Generated password: %s\n", password);
-
-    return 0;
+        return (0);
 }
