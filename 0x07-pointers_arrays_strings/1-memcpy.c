5b7a807f4da866f8fcd77e7f02ee41722a9317a1
Myksdark <myksdark@gmail.com>
2023-04-03 16:33:01 +0000

    Copies mem area

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -7,11 +7,9 @@
  * @n: number of bytes to copy
  * Return: pointer to dest
  */
-
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-    
-    unsigned int i;    
+    unsigned int i;
     for (i = 0; i < n; i++)
     {
         dest[i] = src[i];
