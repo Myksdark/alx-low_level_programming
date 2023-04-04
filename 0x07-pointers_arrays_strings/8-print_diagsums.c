49b75de825f3f8ab4752002fe8f0144ebe6074be
myksdark <myksdark@gmail.com>
2023-04-04 18:29:20 +0000

    Print sum of two lines

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -8,15 +8,13 @@
  */
 void print_diagsums(int *a, int size)
 {
-int i;
-int sum1 = 0;
-int sum2 = 0;
+int b, sum1 = 0, sum2 = 0;
 
-for (i = 0; i < size; i++)
-{
-sum1 += *(a + i * size + i);
-sum2 += *(a + i * size + (size - i - 1));
-}
+	for (b = 0; b < size; b++)
+	{
+		sum1 += a[(size + 1) * b];
+		sum2 += a[(size - 1) * (b + 1)];
+	}
 
-printf("%d,%d\n", sum1, sum2);
+	printf("%d, %d\n", sum1, sum2);
 }
