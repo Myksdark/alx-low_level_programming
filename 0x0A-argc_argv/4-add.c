9437159cfcda63125c7a2fb6b5d2273570d236b1
Myksdark <myksdark@gmail.com>
2023-04-15 02:39:58 +0000

    To infinity and beyond

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -9,28 +9,22 @@
  *
  * Return: 0 on success,and 1 on error
  */
-int add_positive_numbers(int argc, char *argv[]) {
-int sum = 0;
-for (int i = 1; i < argc; i++) {
-for (int j = 0; argv[i][j] != '\0'; j++) {
-if (!isdigit(argv[i][j])) {
-printf("Error\n");
-return 1;
-}
-}
-sum += atoi(argv[i]);
-}
-return sum;
-}
+int main(int argc, char *argv[])
+{
+int i, j, add = 0;
 
-int main(int argc, char *argv[]) {
-if (argc <= 1) {
-printf("0\n");
-} else {
-int result = add_positive_numbers(argc, argv);
-if (result != 1) {
-printf("%d\n", result);
+for (i = 1; i < argc; i++)
+{
+for (j = 0; argv[i][j] != '\0'; j++)
+{
+if (!isdigit(argv[i][j]))
+{
+printf("Error\n");
+return (1);
 }
 }
-return 0;
+add += atoi(argv[i]);
+}
+printf("%d\n", add);
+return (0);
 }
