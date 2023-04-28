a3d2a936fff148aa8a082a9f4a2e927d84c7efa0
Myksdark <myksdark@gmail.com>
2023-04-28 14:14:13 +0000

    Printed a list

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+ * print_listint - Prints all the elements of a listint_t list.
+ * @h: The head of the list.
+ *
+ * Return: The number of nodes in the list.
+ */
+size_t print_listint(const listint_t *h) {
+size_t num = 0;
+while (h != NULL) {
+printf("%d\n", h->n);
+num++;
+h = h->next;
+}
+return (num);
+}
---
+++ 0x13-more_singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+This directory contains my ALX project on 0x13-more singly linked lists.
